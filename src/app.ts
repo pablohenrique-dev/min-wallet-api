1f31596a424a065caa54edb1acf361a2e7023ef8
Pablo Henrique <pablohdsouza.dev@gmail.com>
2024-06-19 10:09:02 -0300

    docs: add swagger to document the api

--- src/app.ts
+++ src/app.ts
@@ -5,9 +5,10 @@ import cors from "cors";
 import { routes } from "./http/routes";
 import { handleError } from "./http/middlewares/handle-error";
 
-const CORS_ALLOWED_ORIGINS = [
-  "http://localhost:5173",
-]
+import swaggerUI from "swagger-ui-express";
+import swaggerDocument from "../swagger.json";
+
+const CORS_ALLOWED_ORIGINS = ["http://localhost:5173"];
 
 export const app = express();
 
@@ -15,11 +16,12 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: CORS_ALLOWED_ORIGINS, 
-    credentials: true, 
+    origin: CORS_ALLOWED_ORIGINS,
+    credentials: true,
   })
 );
 
 app.use(routes);
+app.use("/api-docs", swaggerUI.serve, swaggerUI.setup(swaggerDocument, { explorer: true }));
 
 app.use(handleError);
